6447e97866b80acc803d0ce520b2ad4d13bc0bdc
azzenovic <azzen.abidi@gmail.com>
2016-07-10 05:50:14 +0100

    Updated Default Database Content

--- src/application.sql
+++ src/application.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 19, 2015 at 11:47 PM
--- Server version: 10.0.16-MariaDB-1
--- PHP Version: 5.6.7-1
+-- Generation Time: Jul 10, 2016 at 06:47 AM
+-- Server version: 5.5.49-MariaDB-1ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `application`
+-- Database: `devbox`
 --
 
 -- --------------------------------------------------------
@@ -27,11 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admins` (
-`admin_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(50) NOT NULL,
   `admin_login` varchar(50) NOT NULL,
-  `admin_password` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `admin_password` varchar(50) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `admins`
@@ -47,20 +48,13 @@ INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_login`, `admin_password`)
 --
 
 CREATE TABLE IF NOT EXISTS `files` (
-`file_id` int(11) NOT NULL,
+  `file_id` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(30) NOT NULL,
-  `file_creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `project_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`file_id`, `file_name`, `file_creation_date`, `project_id`) VALUES
-(7, 'test.pas', '2015-05-12 01:30:58', 6),
-(8, 'test.pas', '2015-05-12 01:41:58', 7),
-(9, 'ban.pas', '2015-05-19 22:20:46', 9);
+  `file_creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`file_id`),
+  KEY `fk_file_project` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -69,18 +63,13 @@ INSERT INTO `files` (`file_id`, `file_name`, `file_creation_date`, `project_id`)
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
-`message_id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL AUTO_INCREMENT,
   `message_content` longtext CHARACTER SET latin1 NOT NULL,
-  `message_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`message_id`, `message_content`, `message_time`, `user_id`) VALUES
-(2, 'hello there help me ! ', '2015-05-12 01:44:49', 60);
+  `message_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`message_id`),
+  KEY `fk_message_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -89,25 +78,13 @@ INSERT INTO `messages` (`message_id`, `message_content`, `message_time`, `user_i
 --
 
 CREATE TABLE IF NOT EXISTS `notifications` (
-`notification_id` int(11) NOT NULL,
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
   `notification_text` varchar(1000) NOT NULL,
-  `notification_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP(),
-  `admin_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`notification_id`, `notification_text`, `notification_time`, `admin_id`) VALUES
-(1, 'this is text ', '2015-03-31 11:20:26', 1),
-(2, 'hello this is warning!!', '2015-03-31 22:14:57', 1),
-(6, 'System', '2015-04-05 14:51:31', 1),
-(7, 'hello', '2015-04-05 14:52:26', 1),
-(8, 'heakakakk', '2015-04-05 14:53:28', 1),
-(9, 'hello', '2015-04-05 14:54:23', 1),
-(10, 'hello', '2015-04-05 14:54:44', 1),
-(15, 'hello test!!!', '2015-05-12 00:39:15', 1);
+  `notification_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `admin_id` int(11) NOT NULL,
+  PRIMARY KEY (`notification_id`),
+  KEY `fk_notifications_admins` (`admin_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 -- --------------------------------------------------------
 
@@ -116,20 +93,13 @@ INSERT INTO `notifications` (`notification_id`, `notification_text`, `notificati
 --
 
 CREATE TABLE IF NOT EXISTS `projects` (
-`project_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(30) NOT NULL,
-  `project_creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `projects`
---
-
-INSERT INTO `projects` (`project_id`, `project_name`, `project_creation_date`, `user_id`) VALUES
-(6, 'taz', '2015-05-12 01:30:39', 59),
-(7, 'pfe', '2015-05-12 01:41:34', 60),
-(9, 'sam', '2015-05-19 22:20:23', 60);
+  `project_creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`project_id`),
+  KEY `fk_project_user` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -138,95 +108,14 @@ INSERT INTO `projects` (`project_id`, `project_name`, `project_creation_date`, `
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) NOT NULL,
   `user_login` varchar(50) NOT NULL,
   `user_password` varchar(50) NOT NULL,
-  `status` varchar(20) NOT NULL DEFAULT 'offline'
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+  `status` varchar(20) NOT NULL DEFAULT 'offline',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `user_name`, `user_login`, `user_password`, `status`) VALUES
-(59, 'devbox ', 'userd', 'ee11cbb19052e40b07aac0ca060c23ee', 'online'),
-(60, 'def user', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'online');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admins`
---
-ALTER TABLE `admins`
- ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `files`
---
-ALTER TABLE `files`
- ADD PRIMARY KEY (`file_id`), ADD KEY `fk_file_project` (`project_id`);
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
- ADD PRIMARY KEY (`message_id`), ADD KEY `fk_message_user` (`user_id`);
-
---
--- Indexes for table `notifications`
---
-ALTER TABLE `notifications`
- ADD PRIMARY KEY (`notification_id`), ADD KEY `fk_notifications_admins` (`admin_id`);
-
---
--- Indexes for table `projects`
---
-ALTER TABLE `projects`
- ADD PRIMARY KEY (`project_id`), ADD KEY `fk_project_user` (`user_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admins`
---
-ALTER TABLE `admins`
-MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `files`
---
-ALTER TABLE `files`
-MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `notifications`
---
-ALTER TABLE `notifications`
-MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT for table `projects`
---
-ALTER TABLE `projects`
-MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=61;
 --
 -- Constraints for dumped tables
 --
@@ -235,25 +124,25 @@ MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=61;
 -- Constraints for table `files`
 --
 ALTER TABLE `files`
-ADD CONSTRAINT `fk_file_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_file_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-ADD CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `notifications`
 --
 ALTER TABLE `notifications`
-ADD CONSTRAINT `fk_notifications_admins` FOREIGN KEY (`admin_id`) REFERENCES `admins` (`admin_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_notifications_admins` FOREIGN KEY (`admin_id`) REFERENCES `admins` (`admin_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `projects`
 --
 ALTER TABLE `projects`
-ADD CONSTRAINT `fk_project_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_project_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
